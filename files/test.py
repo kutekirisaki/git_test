3c793c50572f2f6cb4425cdc0a915de7aeddc820
kutekirisaki <jackie123768@gmail.com>
2021-07-07 01:23:58 -0700

    shortern divide function

--- files/test.py
+++ files/test.py
@@ -1,12 +1,11 @@
 def area(a, b):
     return a*b
     
-def div(x,y):
-    if x > y:
+def div(x,y): 
+    if x >= y:
         return x/y
-    elif y > x:
-        return y/x
     else:
-        return x/y
+        return y/x
 
+        
 print(area(2, 3))
