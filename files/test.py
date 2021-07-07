6885e6b12a332ac5609eb8c2a0de8cd818af5ad4
kutekirisaki <jackie123768@gmail.com>
2021-07-07 01:07:43 -0700

    add dividebranch

---
+++ files/test.py
@@ -0,0 +1,12 @@
+def area(a, b):
+    return a*b
+    
+def div(x,y):
+    if x > y:
+        return x/y
+    elif y > x:
+        return y/x
+    else:
+        return x/y
+
+print(area(2, 3))
--- test.py
+++
@@ -1,4 +0,0 @@
-def area(a, b):
-    return a*b
-
-print(area(2, 3))
